feb356d5a857471845b8515c832b078a975871c9
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-09-19 20:13:37 +0530

    all model field added

--- app_geeky_model_tutorial/models.py
+++ app_geeky_model_tutorial/models.py
@@ -1,3 +1,4 @@
+from distutils import text_file
 import uuid
 from django.utils.translation import gettext_lazy as _
 from django.db import models
@@ -63,3 +64,30 @@ class MyUUIDModel(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class FieldType(models.Model):
+    auto_field_example = models.AutoField(primary_key=True)
+    boolean_field_exaple = models.BooleanField()
+    char_field_example = models.CharField( max_length=50)
+    date_field_exmaple = models.DateField()
+    datetme_field_example = models.DateTimeField(auto_now=False, auto_now_add=False)
+    decimal_example =models.DecimalField(max_digits=5, decimal_places=2)
+    duration_exaple = models.DurationField()
+    email_example = models.EmailField(max_length=254)
+    file_example = models.FileField(upload_to="fieldexample", max_length=100)
+    file_path_example = models.FilePathField(path='', match=None, recursive=False, allow_files=True, allow_folders=False, max_length=100)
+    float_example = models.FloatField()
+    ip_exaple = models.FloatField()
+    imagne_exmaple =models.ImageField(upload_to="modelimage", height_field=None, width_field=None, max_length=None)
+    integer_example = models.IntegerField()
+    json_example = models.JSONField (encoder=None, decoder=None)
+    slug_example = models.SlugField()
+    small_integer_exaple = models.SmallIntegerField()
+    txtfield_example = models.TextField()
+    time_exaple = models.TimeField( auto_now=False, auto_now_add=False)
+    url_example = models.URLField(max_length=200)
+    uuid_eaxmple = models.UUIDField(primary_key=False, default=uuid.uuid4, editable=False)
+    # relation_with_my_uuid_cascade = models.ForeignKey(MyUUIDModel,on_delete=models.CASCADE)
+    # relation_with_my_uuid_protect = models.ForeignKey(Card,on_delete=models.PROTECT)
+    # relation_with_my_uuid_restrict = models.ForeignKey(Student,on_delete=models.RESTRICT)
